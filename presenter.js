896af773622156be26c3144ac8cc0d97c2be71b9
Richard Wong <ricwtk@gmail.com>
2014-05-17 13:12:38 +0100

    create function to fix width for slide carousel; add function to add event listener for slide carousel

--- presenter.js
+++ presenter.js
@@ -10,13 +10,21 @@ function initialise() {
 function setUIpos() {
   document.getElementById('toggleCar').style.fontSize = document.getElementById('toggleCar').offsetHeight*0.7 + 'px';
   document.getElementById('config').style.fontSize = document.getElementById('config').offsetHeight*0.7 + 'px';
+  var styleNew = document.createElement('style');
+  document.head.appendChild(styleNew);
+  styleNew.sheet.insertRule('.slideCarousel {width:' + document.getElementById('carousel').offsetHeight*0.9 *4/3 + 'px}', styleNew.sheet.cssRules.length);
 }
 
 function loadSlide() {
   var json = getJSON('./doc1.json');
   for (var i = 0; i < json.length; i++) {
-    createSlideDiv(json[i], 'slideMain');
-    document.getElementById('carousel').appendChild(createSlideDiv(json[i], 'slideCarousel'));
+    var divMain = createSlideDiv(json[i], 'slideMain');
+    divMain.id = i;
+    var divCarousel = createSlideDiv(json[i], 'slideCarousel');
+    divCarousel.id = i;
+    divCarousel.addEventListener('click', switchToSlide);
+    document.getElementById('slideContainer').appendChild(divMain);
+    document.getElementById('carousel').appendChild(divCarousel);
   }
 }
 
@@ -28,5 +36,12 @@ function toggleCarousel(e) {
   } else {
     control.style.bottom = '0px';
     this.innerHTML = 'Hide slides';
+  }
+}
+
+function switchToSlide(e) {
+  var allSlide = document.getElementsByClassName('slideMain');
+  for (var i = 0; i < allSlide.length; i++) {
+    allSlide[i].style.display = (i == this.id) ? 'block' : 'none';
   }
 }
